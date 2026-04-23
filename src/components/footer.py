5271f79319666b0e4ce500bd18788cba8e2fc1de
Prakhar Garg <satishagrawal6993@gmail.com>
2026-04-23 22:52:25 +0530

    streamlit setup + homescreen

---
+++ src/components/footer.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+def footer_home():
+
+    logo_url = "https://dummyimage.com/600x200/5865F2/ffffff&text=Prakhar"
+
+    st.markdown(f"""
+    <div style="margin-top: 2rem; display:flex; gap:6px; justify-content:center; items-align:center">
+            <p style="font-weight:bold; color:white"> Created by  </p>
+            <img src = '{logo_url}' style='max-height: 25px' />
+    </div>        
+     
+                
+""", unsafe_allow_html=True)
+
